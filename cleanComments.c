8cbcfce21ec9fdd35364c7016aaeb23985d091e9
Lidngren <adamlindgren@outlook.com>
2018-02-28 17:31:05 +0100

    Lagt till kommentarer
    Inte klar dock

--- cleanComments.c
+++ cleanComments.c
@@ -1,3 +1,17 @@
+/*
+DV2: Algoritmer och problemlösning.
+File: cleanComments.c
+Name: Adam Lindgren & Jakob Lundin.
+CS-user: dv17aln & c14jln
+Date: 26 Februari 2018
+Description: Goes through a given textfile and removes the comments in between
+			the '/*' and '* /' (with no space inbetween, we had to include that
+			because otherwise it would undo this comment).
+input: A filepath for input file and a filepath for output file.
+Output: Writes the the resulting text to the given output file.
+Limitations: None.
+*/
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -21,6 +35,7 @@ int main(int argc, char **argv){
 	c = fgetc(inFile);
 	while(c != EOF){
 		switch(state) {
+			//
 			case 1:
 				if(c == '/') {
 					state = 2;
@@ -28,6 +43,7 @@ int main(int argc, char **argv){
 					fputc(c, outFile);
 				}
 				break;
+			//
 			case 2:
 				if(c == '*') {
 					state = 3;
@@ -37,11 +53,13 @@ int main(int argc, char **argv){
 					fputc(c, outFile);
 				}
 				break;
+			//
 			case 3:
 				if(c == '*') {
 					state = 4;
 				}
 				break;
+			//
 			case 4:
 				if(c == '/') {
 					state = 1;
@@ -57,6 +75,13 @@ int main(int argc, char **argv){
 	fclose(outFile);
 }
 
+
+/* Function:	openFile
+ * Description:	Opens a file, if not possible it exits the program and gives
+ 				an error message.
+ * Input:		A string with the filepath.
+ * Output:		File pointer.
+ */
 FILE *openFile(char *file, char *mode){
 	FILE *fp = fopen(file, mode);
 	if(fp == NULL) {
